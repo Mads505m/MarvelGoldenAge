2c04ebd667ef4b06c682909b202b06d287fc9244
minkguel <mikkel.ostvand.pedersen@gmail.com>
2024-10-30 18:18:13 +0100

    swagger comments

--- routes/HeroRoutes.js
+++ routes/HeroRoutes.js
@@ -37,10 +37,66 @@ router.get('/heroes', heroController.getAllHeroes);
  */
 router.get('/heroes/:id',heroController.getSpecificHero);
 
+/**
+ * @swagger
+ * /heroes:
+ *   post:
+ *     summary: Opret en ny helt
+ *     description: Opretter en ny helt.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Hero'
+ *     responses:
+ *       201:
+ *         description: Helt oprettet.
+ *       400:
+ *         description: Ugyldige input.
+ */
 router.post('/heroes', heroController.createHero);
 
+/**
+ * @swagger
+ * /heroes/{id}:
+ *   delete:
+ *     summary: Slet en helt
+ *     description: Sletter en helt baseret på den angivne ID.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID for den helt, der skal slettes.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Helt slettet.
+ *       404:
+ *         description: Helt ikke fundet.
+ */
 router.delete('/heroes/:id', heroController.deleteHero);
 
+/**
+ * @swagger
+ * /heroes/{id}:
+ *   put:
+ *     summary: Opdater en helt
+ *     description: Opdaterer en helt baseret på det angivne ID.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID for den helt, der skal opdateres.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *          description: Helt opdateret.
+ *       404:
+ *          description: Helt ikke fundet.
+ */
 router.put('/heroes/:id', heroController.updateHero);
 
 module.exports = router;
